772c045f7c07335a2fb5bfa13de9f2fc72dd7860
Antoine Taillefer <ataillefer@nuxeo.com>
2020-01-14 10:25:59 +0100

    NXBT-3199: Fix updatebot regex

--- updatebot.sh
+++ updatebot.sh
@@ -2,7 +2,7 @@
 
 VERSION=$1
 
-jx step create pr regex --regex "^(?m)\s+Image: .*\/platform-jenkinsx\s+ImageTag: (.*)$" \
+jx step create pr regex --regex "^(?m)\s+Image: .*\/.*-jenkinsx\s+ImageTag: (.*)$" \
   --version ${VERSION} \
   --files values.yaml \
   --repo https://github.com/nuxeo/jx-platform-env.git
